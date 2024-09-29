aad05b78662dd28b5ac1b00b8047ad9447464cad
Willian Brito <willian_brito00@hotmail.com>
2024-09-28 21:07:29 -0300

    fix: hover do dropdown do usuário

--- src/components/template/UserDropdown.vue
+++ src/components/template/UserDropdown.vue
@@ -1,15 +1,13 @@
 <template>
-    <div class="user-dropdown">
+    <div class="user-dropdown" @click="showUserDropdown">
         <div class="user-button">
             <span class="d-none d-sm-block">{{ user.name }}</span>
             <div class="user-dropdown-img avatar avatar-online">
                 <Gravatar :email="user.email" alt="Usuário"/>
             </div>
-            <i class="fa fa-angle-down"></i>
+            <i :class="isVisible ? 'fa fa-angle-down' : 'fa fa-angle-up'"></i>
         </div>
-        <div class="user-dropdown-content">
-            <!-- <a href><i class="fa fa-cogs"></i>Administração</a>
-            <a href><i class="fa fa-sign-out"></i>Sair</a> -->
+        <div :class="['user-dropdown-content', { 'show': isVisible }]">
 
             <li class="nav-item navbar-dropdown dropdown-user dropdown">
                 <ul class="dropdown-menu dropdown-menu-end show">
@@ -35,7 +33,13 @@
                     </a>
                 </li>
                 <li>
-                    <a class="dropdown-item" href="#"> <i class="bx bx-cog bx-md"></i><span>Configurações</span> </a>
+                    <router-link to="/admin" class="dropdown-item">
+
+                        <!-- <a class= href="#">  -->
+                            <i class="bx bx-cog bx-md"></i>
+                            <span>Configurações</span> 
+                        <!-- </a> -->
+                    </router-link>
                 </li>
                 <li>
                     <div class="dropdown-divider my-1"></div>
@@ -58,7 +62,17 @@ import Gravatar from 'vue-gravatar'
 export default {
     name: 'UserDropdown',
     components: { Gravatar },
-    computed: mapState(['user'])
+    data() {
+        return {
+            isVisible: false
+        }
+    },
+    computed: mapState(['user']),
+    methods: {
+        showUserDropdown() {
+            this.isVisible = !this.isVisible;
+        }
+    }
 }
 </script>
 
@@ -78,10 +92,6 @@ export default {
         padding: 0px 5px;
     }
 
-    .user-dropdown:hover {
-        background-color: var(--bs-btn-hover);
-    }
-
     .user-dropdown-img {
         margin: 0px 10px;
     }
@@ -93,6 +103,7 @@ export default {
 
     .user-dropdown-content {
        position: absolute;
+       margin-top: 15px;
        right: 30px;
        background-color: var(--bs-white);
        min-width: 170px;
@@ -109,15 +120,14 @@ export default {
        transition: visibility 0s, opacity 0.5s linear;
     }
 
-    .user-dropdown:hover .user-dropdown-content {
-        visibility: visible;
-        opacity: 1;
+    .user-dropdown-content.show {
+        visibility: visible !important;
+        opacity: 1 !important;
     }
 
     .user-dropdown-content a {
         text-decoration: none;
         color: var(--bs-navbar-hover-color);
-        padding: 5px;
     }
 
     .user-dropdown-content a:hover {
@@ -128,5 +138,5 @@ export default {
 
     .user-dropdown-content a i {
         margin: 0px 5px;
-    }
+    } 
 </style>
